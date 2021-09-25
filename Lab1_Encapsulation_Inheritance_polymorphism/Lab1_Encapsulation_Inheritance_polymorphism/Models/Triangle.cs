f26596c2c74484954816fe0f92d74f29a4296e61
KhomenkoMac <a.kaverin@pnn.in.ua>
2021-09-25 23:00:42 +0300

    add: props for Triangle

--- Lab1_Encapsulation_Inheritance_polymorphism/Lab1_Encapsulation_Inheritance_polymorphism/Models/Triangle.cs
+++ Lab1_Encapsulation_Inheritance_polymorphism/Lab1_Encapsulation_Inheritance_polymorphism/Models/Triangle.cs
@@ -8,6 +8,34 @@ namespace Lab1_Encapsulation_Inheritance_polymorphism.Models
 {
 	public class Triangle : Polygon
 	{
+		private double _aSide;
+		public double A_Side
+		{
+			get { return _aSide; }
+			set { _bSide = value; }
+		}
+
+		private double _bSide;
+		public double B_Side
+		{
+			get { return _bSide; }
+			set { _bSide = value; }
+		}
+
+		private double _cSide;
+		public double C_Side
+		{
+			get { return _cSide; }
+			set { _cSide = value; }
+		}
+
+		public Triangle(double a, double b, double c)
+		{
+			_aSide = a;
+			_bSide = b;
+			_cSide = c;
+		}
+
 		public override double Square { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
 	}
 }
