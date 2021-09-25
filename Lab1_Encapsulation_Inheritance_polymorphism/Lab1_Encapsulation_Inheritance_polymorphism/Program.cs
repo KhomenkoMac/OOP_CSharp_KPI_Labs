851ff20f5c9a14ca45a2d12b29fce8f451d199db
KhomenkoMac <a.kaverin@pnn.in.ua>
2021-09-25 22:33:02 +0300

    init project

---
+++ Lab1_Encapsulation_Inheritance_polymorphism/Lab1_Encapsulation_Inheritance_polymorphism/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lab1_Encapsulation_Inheritance_polymorphism
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+	}
+}
