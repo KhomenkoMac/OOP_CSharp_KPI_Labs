43300ad49b5b397050a8c82981d0add11242764f
KhomenkoMac <maksim_khomenko@ukr.net>
2021-12-11 18:13:58 +0300

    add:implem. CoR pattern

---
+++ Lab5/Part1_ChainOfResponsibilities/Program.cs
@@ -0,0 +1,71 @@
+﻿using Part2_ChainOfResponsibilities;
+using System;
+
+namespace Part1_ChainOfResponsibilities
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var owner = new Person()
+            {
+                Firstname = "Tim",
+                Lastname = "Cook",
+                HaveDesireForShow = false,
+                Balance = 110
+            };
+
+            Ticket t = new()
+            {
+                ShowName = "Сто тисяч",
+                Price = 200,
+                Owner = owner
+            };
+
+
+            Person p1 = new()
+            {
+                Firstname = "Tim1",
+                Lastname = "Cook1",
+                Ticket = null,
+                HaveDesireForShow = true,
+                Balance = 1000
+            };
+
+            Person p2 = new()
+            {
+                Firstname = "Tim2",
+                Lastname = "Cook2",
+                Ticket = null,
+                HaveDesireForShow = false,
+                Balance = 1000
+            };
+
+            Person p3 = new()
+            {
+                Firstname = "Tim3",
+                Lastname = "Cook3",
+                Ticket = null,
+                HaveDesireForShow = true,
+                Balance = 1250
+            };
+
+            Console.WriteLine($"Your balance: {owner.Balance}");
+
+            t.Owner.SetNextNexus(p1).SetNextNexus(p3).SetNextNexus(p2);
+            bool sold = t.Owner.ProcessBargain(t);
+            
+            if (!sold)
+            {
+                Console.WriteLine("Not sold!");
+            }
+            else
+            {
+                Console.WriteLine($"Sold to {t.Owner}");
+            }
+            
+            Console.WriteLine($"Your balance: {owner.Balance}");
+            Console.WriteLine($"not Your balance: {t.Owner.Balance}");
+        }
+    }
+}
---
+++ Lab5/Part1_ChainOfResponsibilities/models/Person.cs
@@ -0,0 +1,103 @@
+﻿using System;
+
+namespace Part2_ChainOfResponsibilities
+{
+    public interface IHandler
+    {
+        IHandler SetNextNexus(IHandler handler);
+        bool ProcessBargain(Ticket initOwner_Ticket);
+    }
+    public abstract class BaseHandler: IHandler
+    {
+        private IHandler _approver;
+
+        public virtual bool ProcessBargain(Ticket initOwner_Ticket)
+        {
+            if (_approver == null)
+            {
+                return false;
+            }
+            else
+            {
+                return _approver.ProcessBargain(initOwner_Ticket);
+            }
+        }
+
+        public IHandler SetNextNexus(IHandler approver)
+        {
+            _approver = approver;
+            return _approver;
+        }
+    }
+
+    public class Ticket
+    {
+        public string ShowName { get; set; }
+        public decimal Price { get; set; }
+        public Person Owner { get; set; }
+    }
+
+    public class Person: BaseHandler
+    {
+        private string _firstname;
+        public string Firstname
+        {
+            get { return _firstname; }
+            set { _firstname = value; }
+        }
+
+        private string _lastname;
+        public string Lastname
+        {
+            get { return _lastname; }
+            set { _lastname = value; }
+        }
+
+        private bool _haveDesireForShow;
+        public bool HaveDesireForShow
+        {
+            get { return _haveDesireForShow; }
+            set { _haveDesireForShow = value; }
+        }
+
+        private Ticket _ticket;
+        public Ticket Ticket
+        {
+            get { return _ticket; }
+            set { _ticket = value; }
+        }
+
+        private decimal _balance;
+        public decimal Balance
+        {
+            get { return _balance; }
+            set { _balance = value; }
+        }
+
+
+        public override bool ProcessBargain(Ticket ticket)
+        {
+            if (_ticket is null && HaveDesireForShow)
+            {
+                _balance -= ticket.Price;
+                ticket.Owner.Balance += ticket.Price;
+                
+                _ticket = ticket;
+                ticket.Owner.Ticket = null;
+
+                ticket.Owner = this;
+
+                return true;
+            }
+            else
+            {
+                return base.ProcessBargain(ticket);
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"{Firstname} {Lastname}";
+        }
+    }
+}
