e5b0939bca152d77dad1dbc85a34489db3fbfca2
KhomenkoMac <maksim_khomenko@ukr.net>
2021-12-11 18:16:01 +0300

    add:reimplemening part1 - templete method

---
+++ Lab5/TempleteMethod/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace TempleteMethod
+{
+    public abstract class Shop
+    {
+        public void Run()
+        {
+            GreetCameClient();
+            ShowAdressInfo();
+            ShowGoods();
+            GetClientPurchase();
+            GetMoneyForGoods();
+            DeliverGoods();
+        }
+
+        protected abstract void GreetCameClient();
+        protected abstract void ShowAdressInfo();
+        protected abstract void ShowGoods();
+        protected abstract void GetClientPurchase();
+        protected abstract void GetMoneyForGoods();
+        protected abstract void DeliverGoods();
+
+    }
+
+    class Program
+    {
+        private enum ShopVisit { Internet = 1, Real }
+
+        private static void DisplayShopToVisit()
+        {
+            Console.WriteLine();
+            Console.WriteLine($"{(int)ShopVisit.Internet} - {ShopVisit.Internet}");
+            Console.WriteLine($"{(int)ShopVisit.Real} - {ShopVisit.Real}");
+            Console.WriteLine();
+        }
+        private static ShopVisit GetShopVisit()
+        {
+            Console.Write("What shop to visit this time? >");
+            return (ShopVisit)int.Parse(Console.ReadLine());
+        }
+
+        static void Main(string[] args)
+        {
+            Shop shop = null;
+            do
+            {
+                try
+                {
+                    DisplayShopToVisit();
+                    switch (GetShopVisit())
+                    {
+                        case ShopVisit.Internet:
+                            shop = new InternetShop();
+                            break;
+                        case ShopVisit.Real:
+                            shop = new RealShop();
+                            break;
+                        default:
+                            throw new InvalidOperationException("Invalid choice!");
+                    }
+                }
+                catch (Exception err)
+                {
+                    Console.WriteLine(err.Message);
+                }
+            } while (true);
+        }
+    }
+}
